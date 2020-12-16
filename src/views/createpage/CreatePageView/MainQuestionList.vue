5e9a1dd1c9a171f2538628c05be08248eeea8792
Jiangnianqi <857015447@qq.com>
2020-12-16 15:56:02 +0800

    添加问卷分数前端

--- src/views/createpage/CreatePageView/MainQuestionList.vue
+++ src/views/createpage/CreatePageView/MainQuestionList.vue
@@ -56,6 +56,7 @@
                       :grade-max="item.gradeMax"
                       :number-type="item.numberType"
                       :question-options="item.questionOptions"
+                      :question-options-score="item.questionOptionsScore"
                       :text-description="item.textDescription"
                       @clickUnSelected="selectOneBox(index)"
                       @clickSelected="selectOneBox(index)"
@@ -194,6 +195,8 @@
                     questionDescription: data['questionDescription'],
                     questionType: data['questionType'],
                     questionOptions: data['questionOptions'],
+                  //
+                    questionOptionsScore: data['questionOptionsScore'],
                     frontOptions: data['frontOptions'],
                     frontChoose: data['frontChoose'],
                     numberType: data['numberType'],
@@ -225,6 +228,7 @@
                     questionDescription: "",
                     questionType: type,
                     questionOptions: [''],
+                    questionOptionsScore: [''],
                     frontOptions: [[]],
                     frontChoose: false,
                     numberType: 'integer',
